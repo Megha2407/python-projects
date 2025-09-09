028c296500895d7d9d54ca1bf7d734b3791e242a
Megha2407 <meghamithra2007@gmail.com>
2025-09-09 20:47:06 +0530

    uppadted

--- pig.py
+++ pig.py
@@ -6,4 +6,16 @@ def roll():
     roll= random.randint(min_value,max_value)
 
     return roll
+while True:
+    players = input("Enter number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2<= players <=4:
+            break
+        else:
+            print("must be btw the 2<= and <=4")
+    else:
+        print("Invaild,try again")
+
+        
 
