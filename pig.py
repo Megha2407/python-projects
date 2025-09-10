f8fb47953d9e8ebb426d663eb66d6a1d97c19b37
Megha2407 <meghamithra2007@gmail.com>
2025-09-10 23:16:29 +0530

    updated

--- pig.py
+++ pig.py
@@ -16,6 +16,43 @@ while True:
             print("must be btw the 2<= and <=4")
     else:
         print("Invaild,try again")
-
         
+max_score = 50
+players_score =[0 for _ in range(players)]
+
+while max(players_score) < max_score:
+     for player_indx in range(players):
+        current_score = 0
+        
+        while True:
+            should_roll = input("would you like to roll? (y/n): ")
+            if should_roll.lower()!= "y":
+                break
+            value = roll()
+            if value == 1:
+                print("you rolled a 1, your turn is over")
+                break
+            else:
+                current_score += value
+                print("you rolled a", value)
+            print("your current score is", current_score)
+        players_score[player_indx] += current_score
+        print("player", player_indx + 1, "your total score is", players_score[player_indx])
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
